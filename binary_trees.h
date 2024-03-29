015f80f07a7f97f86ef3d4184de55cb08ab06fb9
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-03-29 11:15:24 +0100

    function that creates a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -5,12 +5,12 @@
 
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
- */
+ *
+*/
 typedef struct binary_tree_s
 {
 	int n;
