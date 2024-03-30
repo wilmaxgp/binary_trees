7b94417879a991b4c35a915e0569b8a9deb921d6
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-03-30 18:43:34 +0100

    function that inserts a value in an AVL Tree

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -9,30 +9,30 @@
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-    if (*tree == NULL)
-    {
-        *tree = binary_tree_node(NULL, value);
-        return *tree; // Return root if tree is empty
-    }
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		if (*tree == NULL)
+			return (NULL);
+		return (*tree);
+	}
 
-    if (value < (*tree)->n)
-    {
-        (*tree)->left = avl_insert(&((*tree)->left), value);
-        if ((*tree)->left == NULL)
-            return NULL; // Allocation failure
-    }
-    else if (value > (*tree)->n)
-    {
-        (*tree)->right = avl_insert(&((*tree)->right), value);
-        if ((*tree)->right == NULL)
-            return NULL; // Allocation failure
-    }
-    else
-    {
-        // Handle duplicate values according to your strategy
-        // For example, you may choose to disallow duplicates
-        // return NULL;
-    }
+	if (value < (*tree)->n)
+	{
+		(*tree)->left = avl_insert(&((*tree)->left), value);
+		if ((*tree)->left == NULL)
+			return (NULL);
+	}
+	else if (value > (*tree)->n)
+	{
+		(*tree)->right = avl_insert(&((*tree)->right), value);
+		if ((*tree)->right == NULL)
+			return (NULL);
+	}
+	else
+		return (NULL);
 
-    // Re-balance the tree after insertion
-    binary_tree_balance(*tree);
+	binary_tree_balance(*tree);
+
+	return (*tree);
+}
