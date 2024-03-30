3025ccb188e4c15b0cde30fd6ec367d3c704825a
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-30 11:50:09 -0700

    fixed errors

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -9,55 +9,55 @@
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-        int balance;
+	int balance;
 
-        if (*tree == NULL)
-        {
-            *tree = binary_tree_node(NULL, value);
-            if (*tree == NULL)
-                return (NULL);
-        return (*tree);
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		if (*tree == NULL)
+			return (NULL);
+		return (*tree);
         }
 
-        if (value < (*tree)->n)
-        {
-            (*tree)->left = avl_insert(&((*tree)->left), value);
-            if ((*tree)->left == NULL)
-                return (NULL);
+	if (value < (*tree)->n)
+	{
+		(*tree)->left = avl_insert(&((*tree)->left), value);
+		if ((*tree)->left == NULL)
+			return (NULL);
         }
-        else if (value > (*tree)->n)
+	else if (value > (*tree)->n)
         {
-            (*tree)->right = avl_insert(&((*tree)->right), value);
-            if ((*tree)->right == NULL)
-                return (NULL);
+		(*tree)->right = avl_insert(&((*tree)->right), value);
+		if ((*tree)->right == NULL)
+			return (NULL);
         }
-        else
-            return (NULL);
+	else
+		return (NULL);
 
     /* Update the height and balance factor */
-    balance = binary_tree_balance(*tree);
+	balance = binary_tree_balance(*tree);
 
     /* Left Left Case */
-    if (balance > 1 && value < (*tree)->left->n)
-        return binary_tree_rotate_right(*tree);
+	if (balance > 1 && value < (*tree)->left->n)
+		return (binary_tree_rotate_right(*tree));
 
     /* Right Right Case */
-    if (balance < -1 && value > (*tree)->right->n)
-        return binary_tree_rotate_left(*tree);
+	if (balance < -1 && value > (*tree)->right->n)
+		return (binary_tree_rotate_left(*tree));
 
     /* Left Right Case */
-    if (balance > 1 && value > (*tree)->left->n)
-    {
-        (*tree)->left = binary_tree_rotate_left((*tree)->left);
-        return binary_tree_rotate_right(*tree);
-    }
+	if (balance > 1 && value > (*tree)->left->n)
+	{
+		(*tree)->left = binary_tree_rotate_left((*tree)->left)
+			return (binary_tree_rotate_right(*tree));
+	}
 
     /* Right Left Case */
-    if (balance < -1 && value < (*tree)->right->n)
-    {
-        (*tree)->right = binary_tree_rotate_right((*tree)->right);
-        return binary_tree_rotate_left(*tree);
-    }
+	if (balance < -1 && value < (*tree)->right->n)
+	{
+		(*tree)->right = binary_tree_rotate_right((*tree)->right);
+		return (binary_tree_rotate_left(*tree));
+	}
 
-    return *tree;
+	return (*tree);
 }
