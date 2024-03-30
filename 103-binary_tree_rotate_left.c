8534a242fb71c18ed7fc2ac7233379a68f13df8a
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-30 11:17:29 -0700

    fix error of betty styling

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,27 +8,27 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new_root;
+	binary_tree_t *new_root;
 
-    if (tree == NULL || tree->right == NULL)
-        return (NULL);
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
 
-    new_root = tree->right;
-    tree->right = new_root->left;
-    if (new_root->left != NULL)
-        new_root->left->parent = tree;
+	new_root = tree->right;
+	tree->right = new_root->left;
+	if (new_root->left != NULL)
+		new_root->left->parent = tree;
 
-    new_root->parent = tree->parent;
-    if (tree->parent != NULL)
-    {
-        if (tree->parent->left == tree)
-            tree->parent->left = new_root;
-        else
-            tree->parent->right = new_root;
-    }
+	new_root->parent = tree->parent;
+	if (tree->parent != NULL)
+	{
+		if (tree->parent->left == tree)
+			tree->parent->left = new_root;
+		else
+			tree->parent->right = new_root;
+	}
 
-    new_root->left = tree;
-    tree->parent = new_root;
+	new_root->left = tree;
+	tree->parent = new_root;
 
-    return (new_root);
+	return (new_root);
 }
