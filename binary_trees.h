565256b64ea8306f33b0410f3eb513d81ddc4212
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-03-30 04:28:49 +0100

    function that inserts a value in a Binary Search Tree

--- binary_trees.h
+++ binary_trees.h
@@ -12,12 +12,15 @@
  */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s bst_t;
+
+/* Function Prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
