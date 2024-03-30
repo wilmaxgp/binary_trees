2912d4d08d533136173083218db3a56aaf2f2ba6
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-03-30 04:35:29 +0100

    function that builds a Binary Search Tree from an array

--- binary_trees.h
+++ binary_trees.h
@@ -18,6 +18,13 @@ typedef struct binary_tree_s
     struct binary_tree_s *right;
 } binary_tree_t;
 
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 typedef struct binary_tree_s bst_t;
 
 /* Function Prototypes */
@@ -48,5 +55,6 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
 
 #endif /* _BINARY_TREES_H_ */
